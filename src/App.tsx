c3d931df4c4cd4c9970ec1b9a039782966b92827
Lisa Lin <66091764+Tapiocaba@users.noreply.github.com>
2025-03-12 13:52:35 -0400

    create tsx + vite app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Gallery } from './components/Gallery'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Art Gallery</h1>
+        <p>Welcome to my collection of artwork</p>
+      </header>
+      <main>
+      </main>
+    </div>
+  )
+}
+
+export default App
